4d8ea4efb63d42e4f3a6230e919187b82a615534
Talis Eduardo Cipriani <tec-talis@MacBook-Air-de-Talis.local>
2023-09-02 13:01:20 -0300

    tabs

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
-import { SafeAreaView, StatusBar } from 'react-native';
+import { NativeBaseProvider, StatusBar } from 'native-base';
+
+import { TEMAS } from './src/estilos/temas';
+import Rotas from './src/Rotas';
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <StatusBar />
-    </SafeAreaView>
+    <NativeBaseProvider theme={TEMAS}>
+      <StatusBar backgroundColor={TEMAS.colors.blue[800]} />
+      <Rotas />
+    </NativeBaseProvider>
   );
 }
