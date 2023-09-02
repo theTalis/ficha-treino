5302f93400312ef7e7b80a3ddb859ce964ac3d2b
Talis Eduardo Cipriani <tec-talis@MacBook-Air-de-Talis.local>
2023-09-02 12:48:40 -0300

    tabs

--- src/Tabs/index.tsx
+++ src/Tabs/index.tsx
@@ -1,10 +1,10 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Ionicons from 'react-native-vector-icons/Ionicons'
+// import Ionicons from 'react-native-vector-icons/Ionicons'
 
-import Principal from "./Principal";
-import Consultas from "./Consultas";
-import Explorar from "./Explorar";
-import Perfil from "./Perfil";
+// import Principal from "./Principal";
+// import Consultas from "./Consultas";
+// import Explorar from "./Explorar";
+// import Perfil from "./Perfil";
 
 const Tab = createBottomTabNavigator()
 
@@ -17,45 +17,45 @@ const screenOptions = {
 }
 
 const tabs = [
-  {
-    name: 'Principal',
-    component: Principal,
-    icon: 'home'
-  },
-  {
-    name: 'Consultas',
-    component: Consultas,
-    icon: 'calendar'
-  },
-  {
-    name: 'Explorar',
-    component: Explorar,
-    icon: 'search'
-  },
-  {
-    name: 'Perfil',
-    component: Perfil,
-    icon: 'person'
-  },
+  // {
+  //   name: 'Principal',
+  //   component: Principal,
+  //   icon: 'home'
+  // },
+  // {
+  //   name: 'Consultas',
+  //   component: Consultas,
+  //   icon: 'calendar'
+  // },
+  // {
+  //   name: 'Explorar',
+  //   component: Explorar,
+  //   icon: 'search'
+  // },
+  // {
+  //   name: 'Perfil',
+  //   component: Perfil,
+  //   icon: 'person'
+  // },
 ]
 
 export default function Tabs() {
-  return (
-    <Tab.Navigator screenOptions={screenOptions}>
-      {tabs.map((tab) => (
-        <Tab.Screen
-          key={tab.name}
-          name={tab.name}
-          component={tab.component}
-          options={{
-            headerShown: false,
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name={tab.icon} color={color} size={size} />
-            )
-          }}
-        />
-      ))
-      }
-    </Tab.Navigator>
-  )
+  // return (
+  //   <Tab.Navigator screenOptions={screenOptions}>
+  //     {tabs.map((tab) => (
+  //       <Tab.Screen
+  //         key={tab.name}
+  //         name={tab.name}
+  //         component={tab.component}
+  //         options={{
+  //           headerShown: false,
+  //           tabBarIcon: ({ color, size }) => (
+  //             <Ionicons name={tab.icon} color={color} size={size} />
+  //           )
+  //         }}
+  //       />
+  //     ))
+  //     }
+  //   </Tab.Navigator>
+  // )
 }
