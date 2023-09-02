942eb10fad3a4bddf31fa0f2fa63be03f53dde3f
Talis Eduardo Cipriani <tec-talis@MacBook-Air-de-Talis.local>
2023-09-02 13:12:59 -0300

    mock

--- src/utils/mock.ts
+++ src/utils/mock.ts
@@ -1,19 +1,113 @@
-const depoimentos = [
+const users = [
   {
-    id: 1,
-    titulo: 'Júlio, 40 anos, São Paulo/SP.',
-    text: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.',
-  },
-  {
-    id: 2,
-    titulo: 'Júlio, 40 anos, São Paulo/SP.',
-    text: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.',
-  },
-  {
-    id: 3,
-    titulo: 'Júlio, 40 anos, São Paulo/SP.',
-    text: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.',
+    id: "user1",
+    token: "adwawdawdawd"
   }
 ]
 
-export { depoimentos }
+const divisoes = [
+  {
+    id: 1,
+    titulo: 'Peito',
+  },
+  {
+    id: 2,
+    titulo: 'Costas',
+  },
+  {
+    id: 3,
+    titulo: 'Perna',
+  },
+  {
+    id: 4,
+    titulo: 'Bíceps',
+  },
+  {
+    id: 5,
+    titulo: 'Tríceps',
+  },
+  {
+    id: 6,
+    titulo: 'Ombro',
+  },
+]
+
+const exercicios = [
+  {
+    id: 1,
+    titulo: "Supino reto com halteres",
+    image: "https://www.mundoboaforma.com.br/wp-content/uploads/2020/12/supino-reto-com-halteres.gif",
+    divisao: 1
+  },
+  {
+    id: 2,
+    titulo: "Remada baixa",
+    image: "https://www.mundoboaforma.com.br/wp-content/uploads/2021/09/remada-sentado-com-cabos-e-triangulo-para-costas.gif",
+    divisao: 2,
+  },
+  {
+    id: 3,
+    titulo: "Legpress",
+    image: "https://www.mundoboaforma.com.br/wp-content/uploads/2020/12/pernas-leg-press-45-tradicional.gif",
+    divisao: 3,
+  },
+  {
+    id: 4,
+    titulo: "Rosca halteres",
+    image: "https://www.mundoboaforma.com.br/wp-content/uploads/2022/09/rosca-biceps-direta-com-halteres.gif",
+    divisao: 4,
+  },
+  {
+    id: 5,
+    titulo: "Triceps pulley",
+    image: "https://www.mundoboaforma.com.br/wp-content/uploads/2021/07/triceps-puxada-no-pulley.gif",
+    divisao: 5,
+  },
+  {
+    id: 6,
+    titulo: "Elevação lateral",
+    image: "https://www.mundoboaforma.com.br/wp-content/uploads/2020/12/ombros-elevacao-lateral-de-ombros-com-halteres.gif",
+    divisao: 6,
+  },
+]
+
+const treinos = [
+  {
+    id: 1,
+    exercicio: 1,
+    series: 4,
+    repeticoes: 13
+  },
+  {
+    id: 2,
+    exercicio: 2,
+    series: 4,
+    repeticoes: 13
+  },
+  {
+    id: 3,
+    exercicio: 3,
+    series: 4,
+    repeticoes: 13
+  },
+  {
+    id: 4,
+    exercicio: 4,
+    series: 4,
+    repeticoes: 13
+  },
+  {
+    id: 5,
+    exercicio: 5,
+    series: 4,
+    repeticoes: 13
+  },
+  {
+    id: 6,
+    exercicio: 6,
+    series: 4,
+    repeticoes: 13
+  }
+]
+
+export { users, divisoes, exercicios, treinos }
